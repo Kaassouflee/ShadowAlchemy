c1209cbd7a29ee46b50ed1fa8a4e53cf30c6bd82
Cynthia <cynthia.wiersma@student.nhlstenden.com>
2024-07-24 20:07:29 +0200

    changed it so that it is playable in the browser

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -1,29 +1,26 @@
 extends Control
 
 const LEVEL_BTN = preload("res://assets/level_button.tscn")
-@export_dir var dir_path
 @onready var grid = $MarginContainer/VBoxContainer/GridContainer
 
+var levels = {
+	0: "main",
+	1: "level_2"
+}
+
+#TODO: add locked levels functionality
 func _ready():
-	get_levels(dir_path)
+	get_levels()
 
-func get_levels(path) ->void:
-	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		var index = 1
-		
-		while file_name != "":
-			if file_name.ends_with(".tscn"):
-				create_lvl_button('%s/%s' % [dir.get_current_dir(), file_name], file_name, index)
-				index +=1
-			file_name =dir.get_next()
-		dir.list_dir_end()
-	else:
-		print("An error has occured when trying to access the level path")
+func get_levels() ->void:
+	var index = 1
+	for level in levels:
+		print(level)
+		var level_path = "res://scenes/levels/" + levels[level] + ".tscn"
+		create_lvl_button(level_path, index)
+		index += 1
 
-func create_lvl_button(lvl_path: String, lvl_name: String, index):
+func create_lvl_button(lvl_path: String, index):
 	var btn = LEVEL_BTN.instantiate()
 	btn.level_path = lvl_path
 	var label = btn.get_node("Label")
