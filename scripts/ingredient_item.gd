4bad8c8c3861bd8113d140c868afc7d47b78b5eb
Wim <w.j.stienstra@hotmail.com>
2024-07-26 18:31:54 +0200

    Added potion effects, sound, sign, reset button, fixed shadows and a ton of particles

--- scripts/ingredient_item.gd
+++ scripts/ingredient_item.gd
@@ -5,11 +5,13 @@ extends Sprite2D
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 @onready var characters = %Characters
 
+var ingredients_list
 var potion_logic
 var picked_up = false
 
 func _ready():
 	potion_logic = get_tree().root.get_node("Level/PotionLogic")
+	ingredients_list = get_tree().root.get_node("Level/PotionUI/CanvasLayer/ItemListBackground/ItemList")
 
 func _process(delta):
 	if picked_up:
@@ -28,6 +30,18 @@ func _on_area_2d_area_entered(area):
 		ingredients_list.add_icon_item(load(ingredient_icon))
 		potion_logic.picked_up_ingredients.append(get_ingredient_name(ingredient_icon))
 	
+	var ingredient_icon = ingredient_item.texture.diffuse_texture.get_load_path()
+	var ingredient_name = get_ingredient_name(ingredient_icon)
+	
+	ingredients_list.add_icon_item(load(ingredient_icon))
+	potion_logic.picked_up_ingredients.append(ingredient_name)
+	
+	# Sets tooltip 
+	var count = potion_logic.picked_up_ingredients.size() - 1
+	if count == -1:
+		count = 0
+	ingredients_list.set_item_tooltip(count, ingredient_name)
+	
 func get_ingredient_name(path: String) -> String:
 	var start = path.rfind("/")
 	var end = path.find(".png")
@@ -35,7 +49,6 @@ func get_ingredient_name(path: String) -> String:
 	if start == -1 or end == -1:
 		return ""
 	return path.substr(start + 1, end - start - 1)
-
 
 func _on_ingredient_pickup_finished():
 	self.queue_free()
--- scripts/potion_logic.gd
+++ scripts/potion_logic.gd
@@ -2,29 +2,42 @@ extends Node2D
 
 # Dictionary to map ingredient combinations to potion types
 var potion_recipes = {
-	"berry_herb_fireflower": {"name": "Fire Potion", "effect": "fire"},
-	"berry_herb_iceflower": {"name": "Ice Potion", "effect": "freeze"},
-	"flower_herb_berry": {"name": "Darkness Potion", "effect": "darkness"},
-	"herb_mushroom_teleportflower": {"name": "Teleportation Potion", "effect": "teleport"},
-	"mushroom_flower_berry": {"name": "Jump Potion", "effect": "jump"},
-	"berry_mushroom_shrinkflower": {"name": "Shrink Potion", "effect": "shrink"},
+	"berry_herb_fireflower": {"name": "Fire Potion", "effect": "fire", "needs_collision": true},
+	"berry_herb_iceflower": {"name": "Ice Potion", "effect": "freeze", "needs_collision": true},
+	"flower_herb_berry": {"name": "Wall Potion", "effect": "wall", "needs_collision": false},
+	"berry_herb_mushroom": {"name": "Darkness Potion", "effect": "darkness", "needs_collision": true},
 }
+@onready var player = $"../Characters/Player"
+@onready var potion_raycast = $"../Characters/Player/PotionRay"
+@onready var tilemap = %TileMap
+var particles: PackedScene = preload("res://scenes/potion_particles.tscn")
 
 var picked_up_ingredients = []
 var potion = null
 var ingredients_list
 var potion_list
+var potion_audio
+var cell_position
+var target
 
 func _ready():
-	ingredients_list = get_tree().root.get_node("Level/PotionUI/CanvasLayer/ItemList")
-	potion_list = get_tree().root.get_node("Level/PotionUI/CanvasLayer/PotionList")
+	ingredients_list = get_tree().root.get_node("Level/PotionUI/CanvasLayer/ItemListBackground/ItemList")
+	potion_list = get_tree().root.get_node("Level/PotionUI/CanvasLayer/PotionListBackground/PotionList")
+	potion_audio = get_tree().root.get_node("Level/PotionUI/PotionAudio")
 
 func _process(delta):
 	if picked_up_ingredients.size() == 3:
 		craftPotion()
 	if potion && Input.is_action_just_pressed("trigger"):
-		usePotion(potion["effect"])
+		usePotion(potion_recipes[get_key_by_name(potion)]["effect"])
 
+# Gets potion key based on name
+func get_key_by_name(potion_name: String) -> String:
+	for key in potion_recipes.keys():
+		if potion_recipes[key]["name"] == potion_name:
+			return key
+	return "Potion not found"
+	
 # Gets the array and combines them to check them against the recipes
 func get_combination():
 	var combination = ""
@@ -36,33 +49,86 @@ func get_combination():
 			combination += "_"
 	return combination
 
+# Crafts potion based on the recipes
 func craftPotion():
 	var combination = get_combination()
 	if potion_recipes.has(combination):
-		var potion_name = potion_recipes[combination]
-		print(picked_up_ingredients)
-		print("Crafted potion: " + potion_name["name"])
+		potion = potion_recipes[combination]["name"]
 		ingredients_list.clear()
 		picked_up_ingredients.clear()
-		potion_list.add_icon_item(load("res://assets/alchemy/potions/potion.png"))
+		potion_list.add_item(potion, load("res://assets/alchemy/potions/potion.png"), false)
+		potion_list.set_item_tooltip(0, potion)
 	else:
+		#TODO: show it somewhere
 		print("No known recipe for this combination of ingredients.")
+
+# Set metadata for a specific cell
+func set_tile_metadata(key: String, value: Variant):
+	target = potion_raycast.get_collider()
+	if target:
+		target.set_meta(key, value)
+		potion = null
+		potion_list.clear()
+	else:
+		#TODO: Show it somewhere
+		print("Invalid tile cell position")
+
+func needsColliding(potion):
+	return potion_raycast.is_colliding() && potion_recipes[get_key_by_name(potion)]["needs_collision"]
 
 func usePotion(effect: String):
 	match effect:
 		"freeze":
-			freeze()
+			if needsColliding(potion):
+				freeze(effect)
 		"fire":
-			fire()
+			if needsColliding(potion):
+				fire(effect)
 		"darkness":
-			darkness()
+			if needsColliding(potion):
+				darkness(effect)
+		"wall":
+			if !needsColliding(potion):
+				wall(effect)
 		_:
 			print("Unknown effect")
 
-# TODO: add potion effects, waiting for object in front of player detection
-func freeze():
-	pass
-func fire():
-	pass
-func darkness():
-	pass
+func freeze(effect):
+	potion_audio.play()
+	set_tile_metadata("has_potion", effect)
+	
+func fire(effect):
+	potion_audio.play()
+	set_tile_metadata("has_potion", effect)
+	
+func darkness(effect):
+	potion_audio.play()
+	set_tile_metadata("has_potion", effect)
+	if target.get_meta("has_potion"):
+		if target.find_parent("LightSprite"):
+			# Add particles
+			var particles_inst = particles.instantiate()
+			particles_inst.emitting = true
+			target.get_parent().get_node("SpriteLight").add_child(particles_inst)
+			# Removes lightParticles
+			target.get_parent().get_parent().get_node("LightParticles").queue_free()
+			#Turn off lights
+			target.get_parent().get_node("SpriteLight").set_meta("max_distance", 0)
+			target.get_parent().get_node("SpriteLight").enabled = false
+			target.get_parent().get_node("ShadowLight").enabled = false
+	
+func wall(effect):	
+	potion_audio.play()
+	#TODO: maybe look at what locations are valid? No placing in walls, but I guess it's the player's responsibilty
+	#TODO: find a way to place particles on the ground
+	if !potion_raycast.get_collider():
+		var current_tile: Vector2i = tilemap.local_to_map(player.global_position)
+		# Get target tile Vector2i
+		var direction = player.movement_vector
+		var target_tile: Vector2i = Vector2i(
+			current_tile.x + direction.x,
+			current_tile.y + direction.y,
+		)
+		# Gets coordinates and places wall
+		var target = tilemap.map_to_local(target_tile)
+		tilemap.set_cell(0, Vector2i(target_tile), 1, Vector2i(1, 12))
