30d971d194aa20f6ffe23f3f99e4518742405de7
Wim <w.j.stienstra@hotmail.com>
2024-07-29 11:25:08 +0200

    Removed comments

--- scripts/memory_item.gd
+++ scripts/memory_item.gd
@@ -14,38 +14,6 @@ func _on_area_2d_area_entered(area):
 	if characters.is_player == 1 or characters.is_player == 2:
 		picked_up_players[characters.is_player] = true
 	update_player_count()
-	
-	
-	#if characters.is_player == 1:
-		#picked_up_count =+ 1
-		#player.get_node("AnimatedSprite2D/SpriteLight").set_meta("max_distance", 0)
-		#player.get_node("AnimatedSprite2D/SpriteLight").enabled = false
-		#player.get_node("AnimatedSprite2D/ShadowLight").enabled = false
-	#if characters.is_player == 2:
-		#picked_up_count =+ 1
-		#
-	#if picked_up_count == 2:
-		#GlobalProgression.last_level_beat = GlobalProgression.current_level
-		#var current_memory = get_tree().root.get_node("Level/LevelMemory")
-		#memory_pickup.play()
-		#characters.is_player = 0
-		#is_active_memory = true
-		#
-		#var memoryUi = MemoryUI.instantiate()
-		#get_tree().root.add_child(memoryUi)
-		#var base = memoryUi.get_node("CanvasLayer/ColorRect")
-		#
-		#if !current_memory || !current_memory.memory:
-			#base.get_node("VBoxContainer/MemoryTitle").text = "Coming soon"
-			#base.get_node("VBoxContainer/MemoryText").text = "Coming soon"
-		#else:
-			## Image
-			#base.get_node("VBoxContainer/MemoryImage").texture = load("res://assets/memories/" + current_memory.memory["image"] + ".png")
-			## Title
-			#base.get_node("VBoxContainer/MemoryTitle").text = current_memory.memory["title"]
-			## Text
-			#base.get_node("VBoxContainer/MemoryText").text = current_memory.memory["text"]
-
 
 func _on_area_2d_area_exited(area):
 	if characters.is_player == 1 or characters.is_player == 2:
