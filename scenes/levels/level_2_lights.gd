2b60f97903d486ee650a200490567ab46e6ec1e8
Cynthia <cynthia.wiersma@student.nhlstenden.com>
2024-07-30 15:08:26 +0200

    added level 5

--- scenes/levels/level_2_lights.gd
+++ scenes/levels/level_2_lights.gd
@@ -1,11 +1,22 @@
 extends Node
 
 @onready var light = $Light
+@onready var light_2 = $Light2
+@onready var light_3 = $Light3
+@onready var light_4 = $Light4
+@onready var light_5 = $Light5
+@onready var light_6 = $Light6
+@onready var light_7 = $Light7
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	light.update_radius(3)
-
+	light.update_radius(1)
+	light_2.update_radius(2)
+	light_3.update_radius(3)
+	light_4.update_radius(2)
+	light_5.update_radius(1)
+	light_6.update_radius(1)
+	light_7.update_radius(1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
