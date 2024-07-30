b1082a492be94def4c48ce604820e566b2518e26
Wim <w.j.stienstra@hotmail.com>
2024-07-30 19:34:53 +0200

    Ending fix

--- scripts/ending.gd
+++ scripts/ending.gd
@@ -2,10 +2,6 @@ extends Control
 
 @onready var main_menu = "res://scenes/UI/main_menu.tscn"
 @onready var level_selection = "res://scenes/UI/level_select.tscn"
-@onready var music = $Music
-
-func _ready():
-	music.play()
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file(main_menu)
