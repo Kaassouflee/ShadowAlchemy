fb1207e1e27751ad1c719cee555959573720b2a5
Wim <w.j.stienstra@hotmail.com>
2024-07-31 11:02:12 +0200

    Level 9 big part

--- scenes/levels/level_9_lights.gd
+++ scenes/levels/level_9_lights.gd
@@ -1,10 +1,20 @@
 extends Node
 
 @onready var light = $Light
+@onready var light_2 = $Light2
+@onready var light_3 = $Light3
+@onready var light_4 = $Light4
+@onready var light_5 = $Light5
+@onready var light_6 = $Light6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	light.update_radius(3)
+	light_2.update_radius(3)
+	light_3.update_radius(2)
+	light_4.update_radius(2)
+	light_5.update_radius(1)
+	light_6.update_radius(2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
